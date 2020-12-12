887a74757b1d451174ff4f6be481e11c349735cb
pasta <42817997+posatta@users.noreply.github.com>
2020-12-12 16:04:18 -0500

    Fix index.js
    badUser was still being called

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import flairNewMember from "./flairNewMember"
 import highlightBumps from "./highlightBumps"
 import templates from "./templates"
 
-badUser()
 miscFlairs()
 highlightStaff()
 flairNewMember()
